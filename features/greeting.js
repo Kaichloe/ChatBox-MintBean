257a6b0d752172fbaada1371aa7b5758bc20980a
Kai <kaiyip.ho23@gmail.com>
2020-10-22 21:38:58 -0400

    auto greeting feature

---
+++ features/greeting.js
@@ -0,0 +1,53 @@
+module.exports = function (controller) {
+
+  controller.on("hello", async(bot, message) => {
+    await welcomeMessage(bot, message);
+  });
+
+  controller.on("welcome_back", async(bot, message) => {
+    await welcomeMessage(bot, message);
+  });
+
+  const welcomeMessage = async(bot, message) => {
+    await bot.reply(message, {type: "typing"});
+
+    setTimeout(async () => {
+      await bot.changeContext(message.reference);
+      await bot.reply(message, "Hello there");
+      await bot.reply(message, {type: "typing"});
+    }, 1500)
+    setTimeout(async () => {
+      await bot.changeContext(message.reference);
+      await bot.reply(message, "I am Kaiyip Ho's Automated Bot!");
+      await bot.reply(message, { type: "typing" });
+    }, 2500);
+    setTimeout(async () => {
+      await bot.changeContext(message.reference);
+      await bot.reply(message, {
+        text: "Feel free to ask me questions about my creater!",
+        quick_replies: [
+          {
+            title: "Contact Info",
+            payload: "Contact Info",
+          },
+          {
+            title: "Work History",
+            payload: "Work History",
+          },
+          {
+            title: "Education",
+            payload: "Education",
+          },
+          {
+            title: "Tech Stack",
+            payload: "Tech Stack",
+          },
+          {
+            title: "About Me",
+            payload: "About Me"
+          }
+        ],
+      });
+    }, 3500);
+  }
+};
---
+++ features/resume.js
@@ -0,0 +1,70 @@
+module.exports = {
+  resume = {
+    contact : {
+      firstName: "Kaiyip",
+      preferName: "Kai",
+      lastName: "Ho",
+      email: "kaiyip.ho23@gmail.com",
+      website: "https://kaichloe.github.io/",
+      github: "https://github.com/Kaichloe",
+      location: "New York City",
+    },
+
+    work: [
+      {
+        company: "National Debt Relief",
+        position: ["Senior Account Manager","Team lead"],
+        startDate: "October 2017",
+        endDate: "August 2019",
+        summary: [
+          "Trained, coached and mentored new hires and team members resulting in many to become top employees",
+          "Audited and managed up to 13 team members daily to exceed department expectations as much as 58%",
+          "Interviewed and delivered candidates that exhibited competitiveness and adaptability to department manager",
+          "Spearheaded policies and monthly grading metrics that led to increase of employee retention over 10%",
+        ],
+        accomplishments:" Performance was ranked on a monthly basis and I was ranked top 3 in my first 5 months on the job and was promoted to manage a team of over 13"
+      }
+    ],
+
+    education: [
+      {
+        institution: "App Academy",
+        focus: "Full Stack Software Development",
+        degree: "Certificate",
+        startDate: "November 2019",
+        endDate: "March 2020"
+      },
+      {
+        institution: "Zicklin School of Business",
+        focus: "Finance",
+        degree: "Bachelor of Science",
+        startDate: "August 2012",
+        endDate: "December 2016"
+      }
+    ],
+
+    projects:[
+      {
+        name: "GoblinHood",
+        link: "https://goblinhood.herokuapp.com/#/",
+        repo: "https://github.com/Kaichloe/GoblinHood-",
+        description: "GoblinHood provides the average investor various financial data on specific stocks for free! Users can see the prices of stocks that they are interested in different intervals in the form of a elegent line graph. Users can also see financial news that is affecting the market and specific stocks.",
+      },
+      {
+        name: "JungleGym",
+        link: "https://jg-gym.herokuapp.com/",
+        repo: "https://github.com/rlim92/MERN_project",
+        description: "JungleGym was created for people too busy join a gym. The app provide users free locations to perform their favorite workouts. The app also provides user with tailored workouts based on their selected preferences."
+      },
+      {
+        name: "Kai Says",
+        link: "https://kaichloe.github.io/KaiSays/",
+        repo: "https://github.com/Kaichloe/KaiSays",
+        description: "Kai Says is my rendition of the classical memory game, Simon Says. I styled it with a dark theme to provide less sprain on user interaction. I created a extra game mode to challenge users visually by having the color flashs not match what the correct sequence."
+      },
+      
+    ]
+
+
+  }
+}
